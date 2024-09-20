905916168bf7f10a74132a9a13150cadbc868f2d
Veronika <Veronika@NIKA-PC>
2024-09-20 22:27:47 +0500

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var httpClient = new HttpClient();
+                var response = httpClient.GetAsync("http://localhost:51369/").Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
